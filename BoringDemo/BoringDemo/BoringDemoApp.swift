57da0fd225859b1f82df5d844cf1dcc5169eb24c
Alessia Barone <alessiabarone1410@gmail.com>
2023-11-08 12:23:16 +0100

    Simple Message

---
+++ BoringDemo/BoringDemo/BoringDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoringDemoApp.swift
+//  BoringDemo
+//
+//  Created by Alessia Barone on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BoringDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
